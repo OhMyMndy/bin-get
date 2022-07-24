6a3c17733a284b69167f25faffa7eddf05e2185f
Mandy Schoep <2277717+OhMyMndy@users.noreply.github.com>
2022-07-24 12:20:46 +0000

    Added Deno TypeScript version + tests

---
+++ bin-get.ts
@@ -0,0 +1,223 @@
+#!/usr/bin/env -S deno run --allow-all
+
+// import React from "https://esm.sh/react@17.0.2"
+import { red } from "https://esm.sh/nanocolors@0.1.12";
+import { gunzip, tar, tgz } from "https://deno.land/x/compress@v0.4.4/mod.ts";
+import {
+  copy,
+  readerFromStreamReader,
+} from "https://deno.land/std@0.149.0/streams/conversion.ts";
+
+import yargs from "https://deno.land/x/yargs@v17.5.1-deno/deno.ts";
+import { Arguments } from "https://deno.land/x/yargs@v17.5.1-deno/deno-types.ts";
+import { YargsInstance } from "https://deno.land/x/yargs@v17.5.1-deno/build/lib/yargs-factory.js";
+
+import {
+  emptyDir,
+  expandGlob,
+  expandGlobSync,
+  walkSync,
+} from "https://deno.land/std@0.149.0/fs/mod.ts";
+
+type ApiResult = {
+  message: string | undefined;
+  body: string | undefined;
+  assets: Array<Asset> | undefined;
+};
+type Asset = {
+  browser_download_url: string;
+  name: string;
+  type: "tgz" | "binary";
+};
+
+async function install(githubPackageName: string, version: string) {
+  if (!githubPackageName) {
+    console.log(red("Please provide a package name as the second argument"));
+    Deno.exit(5)
+  }
+
+
+  let githubApiUrl =
+    `https://api.github.com/repos/${githubPackageName}/releases/latest`;
+  if (version) {
+    githubApiUrl =
+      `https://api.github.com/repos/${githubPackageName}/releases/tags/${version}`;
+  }
+
+  let result = await (await api(githubApiUrl)).json() as ApiResult;
+  if (result.message) {
+    console.log(red(`${result.message} for package ${githubPackageName} at ${githubApiUrl}`));
+    Deno.exit(1);
+  }
+  if (result.assets) {
+    appendAssetsFromBody(result);
+    result.assets = result.assets.filter(function (asset: Asset) {
+      return matchAssetToPlatform(asset);
+    });
+
+    if (result.assets.length === 0) {
+      console.log(red(`No downloadable asset found for ${githubPackageName}`));
+      Deno.exit(3);
+    }
+    // if (result.assets.length > 1) {
+    //   console.log(red(`Multiple downloadable assets found for ${githubPackageName}`));
+    //   console.log(result.assets.map((asset) => asset.browser_download_url));
+    // }
+    downloadAsset(result.assets[0], githubPackageName);
+  }
+}
+const systemArch = Deno.build.arch.toLowerCase();
+const archMap: Record<string, string[]> = {
+  "x86_64": ["x86_64", "amd64"],
+};
+
+const os = Deno.build.os.toLowerCase();
+
+function matchAssetToPlatform(asset: Asset): boolean {
+  const assetName = asset.name.toLowerCase();
+  let architectureMatch = false;
+  if (archMap[systemArch]) {
+    for (const arch of archMap[systemArch]) {
+      if (assetName.match(arch)) {
+        architectureMatch = true;
+        break;
+      }
+    }
+  }
+  if (!architectureMatch) {
+    return false;
+  }
+
+  if (!assetName.match(os)) {
+    return false;
+  }
+
+  if (assetName.match(/(tar\.gz|tgz)$/)) {
+    asset.type = "tgz";
+  }
+
+  if (assetName.match(/(\.asc|\.sha[0-9]+(sum)?|\.md5)$/)) {
+    return false;
+  }
+
+  if (!asset.type) {
+    asset.type = "binary";
+  }
+
+  return true;
+}
+
+function appendAssetsFromBody(result: ApiResult) {
+  const urls = result.body?.matchAll(/http[^)]+/g);
+  if (!urls) {
+    return;
+  }
+
+  for (const url of urls) {
+    result.assets?.push({
+      browser_download_url: url[0],
+      name: url[0],
+    } as Asset);
+  }
+}
+async function downloadAsset(
+  asset: Asset,
+  githubPackageName: string,
+
+): Promise<boolean> {
+  verbose && console.log(asset);
+
+  const packageName = githubPackageName.split('/')[1]
+  const tempDir = await Deno.makeTempDir();
+  const tempResult = await Deno.makeTempFile();
+  let filePath: string | null = null
+
+  const response = await api(asset.browser_download_url);
+  const rdr = response.body?.getReader();
+  if (rdr) {
+    const r = readerFromStreamReader(rdr);
+    const f = await Deno.open(tempResult, {
+      create: true,
+      write: true,
+    });
+    await copy(r, f);
+    f.close();
+  }
+
+  if (asset.type == "tgz") {
+    console.log(`uncompressing to ${tempDir}`);
+    await tgz.uncompress(tempResult, tempDir);
+    // now copy the binary to the tempResult binary
+    const files = Array.from(walkSync(tempDir, {
+      includeDirs: false,
+      includeFiles: true,
+    }))
+    for (const file of files) {
+      if (file.name == packageName) {
+        filePath = file.path
+        break
+      }
+    }
+
+  } else if (asset.type == 'binary') {
+    filePath = tempResult
+  }
+  try {
+    if (filePath) {
+      console.log("Installing...")
+      await Deno.chmod(filePath, 0o755)
+      await Deno.copyFile(filePath, "/usr/bin/" + packageName)
+    }
+  } finally {
+    await emptyDir(tempDir);
+    await Deno.remove(tempResult)
+  }
+
+  return true;
+}
+
+async function api(url: string) {
+  const headers: Record<string, string> = {};
+  if (url.match(/github/)) {
+    const credentials = githubCredentials();
+    if (credentials) {
+      headers["Authorization"] = "Basic " + credentials.join(":");
+    }
+  }
+  return await fetch(url, {
+    headers: headers,
+  });
+}
+
+function githubCredentials() {
+  const token = Deno.env.get("GITHUB_TOKEN");
+  const user = Deno.env.get("GITHUB_USER");
+  if (token && user) {
+    return [
+      user,
+      token,
+    ];
+  }
+  return null;
+}
+let verbose = false
+await yargs(Deno.args)
+  .command(
+    "install <package> [version] [--yes] [--force] [--verbose]",
+    "install a package",
+    (yargs: YargsInstance) => {
+      return yargs.positional("package", {
+        describe: "Github repo name (helm/helm for example)",
+      });
+    },
+    (argv: Arguments) => {
+      if (argv.version === true) {
+        argv.version = undefined
+      }
+      verbose = argv.verbose
+      install(argv.package, argv.version);
+    },
+  )
+  .strictCommands()
+  .demandCommand(1)
+  .parse();
