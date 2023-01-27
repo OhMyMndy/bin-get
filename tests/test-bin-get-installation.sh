911ea88d531b8382fb8be6dbe3921d28ddb83285
Mandy Schoep <2277717+OhMyMndy@users.noreply.github.com>
2023-01-27 11:20:38 +0000

    Remove bin-get non TS version

--- tests/test-bin-get-installation.sh
+++ tests/test-bin-get-installation.sh
@@ -12,16 +12,6 @@ set -e
 sudo rm -f "$(command -v bin-get)"
 
 # This is the same code as in the README.md
-
-sudo curl -SsL https://raw.githubusercontent.com/OhMyMndy/bin-get/main/bin-get -o /usr/bin/bin-get
-sudo chmod +x /usr/bin/bin-get
-
-
-if ! command -v bin-get >/dev/null; then
-    echo "bin-get is not installed, but should!" >&2
-    exit 1
-fi
-
 sudo curl -fsSL https://deno.land/install.sh | sudo DENO_INSTALL=/usr/local sh
 
 sudo deno run --allow-all https://raw.githubusercontent.com/OhMyMndy/bin-get/main/bin-get.ts install helm/helm
